4bccd6261f65e469ff7ce4e01fc5223ce67067d0
kurororuciful <omarmanantan27@gmail.com>
2024-09-27 08:00:55 -0500

    week2Activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,8 @@
+//declarations
+const input = document.querySelector("favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("_____");
+
+//create
+const listBOM = document.createElement("ul");
+const deleteButton = document.createElement("button");
