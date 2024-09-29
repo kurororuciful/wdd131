b1e9e9ffe1bac77c4cc54f03529741bcc462f574
kurororuciful <omarmanantan27@gmail.com>
2024-09-29 12:27:33 -0500

    debugging solution

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,6 +1,12 @@
 const PI = 3.14;
-const radius = 3;
+//let radius = 3;
 let area = 0;
-area = radius * radius * pi;
-radius = 4;
-area = radius * radius * pi;
+function circleArea(radius) {
+    const area = radius * radius * PI;
+    return area;
+}
+area = circleArea(3);
+console.log("Area1");
+//radius = 4;
+area = circleArea(4);
+console.log("Area2");
