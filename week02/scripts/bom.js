96373c40ce9717fab27e1809eecf9571a44e9414
kurororuciful <omarmanantan27@gmail.com>
2024-09-29 05:27:55 -0500

    eventhandling

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,25 +3,30 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
+//add button
 button.addEventListener('click', () => {
     const myItem = input.value;
     input.value = '';
 
-    //create
-    const li = document.createElement('li');
-    const listText = document.createElement('span');
-    const deleteButton = document.createElement('button');
+    //trim
+    if (input.value.trim() !== '') {
+            
+        //create
+        const li = document.createElement('li');
+        const listText = document.createElement('span');
+        const deleteButton = document.createElement('button');
 
-    //populate
-    li.appendChild(listText);
-    listText.textContent = myItem;
-    li.appendChild(deleteButton);
-    deleteButton.textContent = '❌';
-    list.appendChild(li);
-
-deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
+        //populate
+        li.appendChild(listText);
+        listText.textContent = myItem;
+        li.appendChild(deleteButton);
+        deleteButton.textContent = '❌';
+        list.appendChild(li);
+    }
+    
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
     });
 
-input.focus();
+    input.focus();
 });
