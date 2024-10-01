9b8d9f2c7299bd262ec3c5b9c96f6b7a158ef8e6
kurororuciful <omarmanantan27@gmail.com>
2024-10-01 05:58:17 -0500

    edits

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,10 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 document.getElementById("lastModified").textContent = new Date(document.lastModified).toLocaleString();
 
 // hamburger button
+const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener("click", () => {
+	mainnav.classList.toggle("show");
+	hambutton.classList.toggle("show");
+});
