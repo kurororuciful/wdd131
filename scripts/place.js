6ef906d73a1455b388e7376768527601a2b38a1e
kurororuciful <omarmanantan27@gmail.com>
2024-10-08 04:18:39 -0500

    windchill

--- scripts/place.js
+++ scripts/place.js
@@ -10,13 +10,12 @@ document.getElementById("lastModified").textContent = new Date(document.lastModi
 // {Wind Chill (°C)} = 13.12 + 0.6215T - 11.37V^{0.16} + 0.3965T(V^{0.16})
 // ( T ) is the air temperature in degrees Celsius.
 // ( V ) is the wind speed in kilometers per hour (kph).
-
-let t = 14.2;
-let v = 59;
-
 function getWindChill(t,v) {
     return 13.12 + 0.6215 * t - 11.37 * (v ** 0.16) + 0.3965 * t * (v ** 0.16);
 }
+
+let t = 14.2;
+let v = 59;
 
 if (t <= 10 && v > 4.8) {
     const windChill = getWindChill(t, v);
