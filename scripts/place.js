4d4c14b22e2e9e38906c69737721e6a989744c10
kurororuciful <omarmanantan27@gmail.com>
2024-10-08 01:51:03 -0500

    final

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,23 @@ const year = document.querySelector("#year");
 const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 document.getElementById("lastModified").textContent = new Date(document.lastModified).toLocaleString();
+
+// windchill factor
+// {Wind Chill (°C)} = 13.12 + 0.6215T - 11.37V^{0.16} + 0.3965T(V^{0.16})
+// ( T ) is the air temperature in degrees Celsius.
+// ( V ) is the wind speed in kilometers per hour (kph).
+
+const t = 14.2;
+const v = 59;
+
+function getWindChill(t,v) {
+    return 13.12 + 0.6215 * t - 11.37 * (v ** 0.16) + 0.3965 * t * (v ** 0.16);
+}
+
+if (t <= 10 && v > 4.8) {
+    const windChill = getWindChill(t, v);
+    document.querySelector("#result").innerHTML = `${windChill.toFixed(2)} °C`;
+} 
+else {
+    document.querySelector("#result").innerHTML = `N/A`;
+}
